e31002a0385253a97f1af0055cd8a45e2ecdee17
SAX_94 <matteosacchi94@gmail.com>
2022-06-21 21:21:07 +0200

    corrected wrong space

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -5,7 +5,7 @@
             <div class="row">
                 <div class="col flex-grow-0 flex-shrink-1 d-flex align-items-center justify-content-between">
                     <div class="p-3">
-                        <img src="/img/dc-logo.png" alt="" />
+                        <img src="/img/dc-logo.png" alt="">
                     </div>
                 </div>
                 <div class="col d-flex align-items-center justify-content-end">
