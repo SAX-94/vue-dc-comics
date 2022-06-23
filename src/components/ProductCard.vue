7e1398927293125ad4cd55569472b16031cc780e
SAX_94 <matteosacchi94@gmail.com>
2022-06-23 19:20:05 +0200

    comics list inserted

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="card border-0">
+        <div class="img-box ratio ratio-1x1">
+            <img :src="imgUrl" :alt="`immagine del fumetto ${cardTitle}`">
+        </div>
+        <div class="card-body text-white text-uppercase">
+            {{ cardTitle }}
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        imgUrl: String,
+        cardTitle: String
+    },
+    data() {
+        return {
+        }
+    },
+};
+</script>
+
+
+<style>
+.card-body {
+    background-color: #1C1C1C;
+    border: 1px solid #1C1C1C;
+    font-size: 13px;
+}
+
+.img-box {
+    width: 100%;
+    overflow: hidden;
+}
+</style>
